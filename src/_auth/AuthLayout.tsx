f20c91c6ab16b3056633a2183318e9656ce27944
sudosuanjal <anjalehackchey101@gmail.com>
2024-02-20 01:54:44 +0530

    introduced shadcn

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,5 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
 const AuthLayout = () => {
-  return <div>AuthLayout</div>;
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+    </div>
+  );
 };
 
 export default AuthLayout;
