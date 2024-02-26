330c2ea70e2965558ba9106115e7a8ef0bb78b6c
sudosuanjal <anjalehackchey101@gmail.com>
2024-02-27 00:52:44 +0530

    created footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,35 @@
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const Footer = () => {
-  return <div className="p-10">Footer</div>;
+  return (
+    <div className="p-10 flex flex-col">
+      <div className="w-30 h-50">
+        <img src="/icons/crowdFlow.svg" />
+      </div>
+      <h3 className="font-bold">Address:</h3>
+      <p className="text-sm">govt.engineering college, wayand</p>
+      <h3 className="font-bold">Contact:</h3>
+      <p className="text-sm">contact@gmail.com</p>
+      <div className="flex gap-4 mt-2">
+        <a href="">
+          <FontAwesomeIcon className="text-2xl" icon={faFacebook} />
+        </a>
+        <a href="">
+          <FontAwesomeIcon className="text-2xl" icon={faInstagram} />
+        </a>
+        <a href="">
+          <FontAwesomeIcon className="text-2xl" icon={faXTwitter} />
+        </a>
+        <a href="">
+          <FontAwesomeIcon className="text-2xl" icon={faLinkedin} />
+        </a>
+      </div>
+    </div>
+  );
 };
